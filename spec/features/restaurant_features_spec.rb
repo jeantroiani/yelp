758adeca449bff957fe1705e9a873af0141c8044
jeantroiani <digitalguest@gmail.com>
2014-08-11 13:24:08 +0100

    Generated controllers for restaurants

--- spec/features/restaurant_features_spec.rb
+++ spec/features/restaurant_features_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
-feature 'Restaurants' do
-	scenario'there is no restaurants showing' do
+feature 'Restaurants have not been added' do
+
+	scenario 'there is no restaurants showing' do
 		visit ('/restaurants')
 		page.should have_content('No restaurants have been added')
+		expect(page).to have_link('Add Restaurant')
+	end
+
+end
+
+feature 'Restaurants have been added' do
+
+	before(:each) do
+		Restaurant.create(name: 'McDonalds')
+	end
+
+	scenario ' Restaurant have been added' do
+		visit ('/restaurants')
+		page.should have_no_content('No restaurants have been added')
+		page.should have_content('McDonalds')
+		expect(page).to have_link('Add Restaurant')
 	end
 
 end
