39ca00cd553d0f3faa8e8595c3a58712176609e9
jeantroiani <digitalguest@gmail.com>
2014-08-12 18:19:51 +0100

    validation fro rating

--- spec/models/reviews_spec.rb
+++ spec/models/reviews_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Review, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+	before(:each) do
+			Restaurant.create(name: 'McDonalds')
+	end
+
+	context 'rating'
+		it 'can not accept > 5 as a rating 'do
+			review = Review.new(rating: -5)
+			expect(review).to have(1).error_on(:rating)		
+		end
+
+	
+		it 'can not accept < 5 as a rating 'do
+			review = Review.new(rating: 6)
+			expect(review).to have(1).error_on(:rating)
+		end
+	end
+	
+
+
